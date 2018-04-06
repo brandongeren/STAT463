e0e36ee1433731d83370d085e94b30359777281c
Brandon Geren <brandongeren@gmail.com>
2018-04-06 15:39:24 -0500

    Added some estimation

--- code/bootstrap.py
+++ code/bootstrap.py
@@ -10,11 +10,12 @@ def values_and_weights(filename):
 
 def bootstrap(values, weights=None, n=1, replace=True):
 	sample = values.sample(n=n, replace=replace, weights=weights)
-	sample = sample.value_counts()
 	sample.to_csv('../bootstrapped_data.csv', index=False)
+	sample = sample.value_counts()
+	sample.to_csv('../bootstrapped_data_value_counts.csv', index=False)
 
 def load_bootstrap_sample(filename):
 	data = pd.read_csv(filename, header=None)
-	indices = np.flip(np.arange(1, len(sample) + 1))
+	indices = np.flip(np.arange(1, len(data) + 1), axis=0)
 	data.index = indices
 	return data
---
+++ code/estimators.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def harmonic_mean(dataframe):
+	
+
+def trimean(dataframe):
+	descriptors = dataframe.describe()
+	q1 = data.describe()[0][4]
+	q2 = data.describe()[0][5]
+	q3 = data.describe()[0][6]
