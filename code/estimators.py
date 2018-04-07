a7dd53975c71c559bb7ff83b8d46444818b2aad2
Brandon Geren <brandongeren@gmail.com>
2018-04-06 22:13:31 -0500

    Finished the trimean estimator

--- code/estimators.py
+++ code/estimators.py
@@ -6,8 +6,9 @@ def harmonic_mean(filename):
 	aggregate_data = load_bootstrap_sample(filename)
 	aggregate_data = aggregate_data.sort_index()
 	counts = aggregate_data[0].values
-	# inverses = list(map(lambda i:1. / (i+1) * counts[i], range(len(counts))))
+	inverses = list(map(lambda i:1. / (i+1) * counts[i], range(len(counts))))
 	harmonic_mean = sum(counts) / sum(inverses)
+	return harmonic_mean
 
 # The argument to this should be the dataframe of the raw bootstrapped data
 def trimean(dataframe):
@@ -15,3 +16,5 @@ def trimean(dataframe):
 	q1 = data.describe()[0][4]
 	q2 = data.describe()[0][5]
 	q3 = data.describe()[0][6]
+	trimean = (q1 + 2 * q2 + q3) / 4
+	return trimean
